675f3f26b296aa85cf6e3effbe27257841a9e263
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-10 08:20:37 -1000

    Friend search box

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
-  resources :user_stocks, only: [:create, :destroy]
+  get 'search_friends', to: 'users#search'
 
+  resources :user_stocks, only: [:create, :destroy]
   resources :users, only: [:show]
   resources :friendships
 end
