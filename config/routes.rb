fa992a9a3397b6a4416b5c0191a268100ff7e954
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-09 15:00:14 -1000

    Add first name and last name to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
 
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
