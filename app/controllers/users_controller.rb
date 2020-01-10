8544e2353d117c706a85ff2910f76b63c0ac6f02
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-09 16:27:45 -1000

    Add friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def my_friends
+    @friendships = current_user.friends
+  end
+
   def my_portfolio
     @user = current_user
     @user_stocks = current_user.stocks
